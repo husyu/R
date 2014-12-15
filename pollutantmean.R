80791f614f4c2dabeb31e7bc73aaceab2a2f5794
husyu <jkem.4ever@gmail.com>
2014-12-14 21:54:11 -0500

    1st step for course submission
    data files and function for mean 1st step only.

---
+++ pollutantmean.R
@@ -0,0 +1,39 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return the mean of the pollutant across all monitors list
+        ## in the 'id' vector (ignoring NA values)
+       
+        
+        if (pollutant != "nitrate" && pollutant != "sulfate") {
+                print("Need to pick 'nitrate' or 'sulfate' for 'pollutant'")
+                return
+                
+        }
+        
+        x <- data.frame(date = character(0), sulfate = numeric(0), nitrate = numeric(0), ID = integer(0))
+        
+        for (i in id) {
+                if (i < 10) {
+                        fileN <- paste("00",i,sep="")
+                } else if (i < 99){
+                        fileN <- paste("0",i,sep="")
+                } else {
+                        fileN <- paste("",i,sep="")
+                }
+                
+                path <- paste("~/R_code/",directory,"/",fileN,".csv",sep="")
+                x <- rbind(x,read.csv(path))
+                View(x)
+                
+        }
+        
+}
