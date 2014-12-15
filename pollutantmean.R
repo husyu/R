3a20309354525e1f9c424fd8f9322d5615ba50d7
husyu <jkem.4ever@gmail.com>
2014-12-14 22:21:38 -0500

    Finished 1st part
    mean by pollutant type and file list

--- pollutantmean.R
+++ pollutantmean.R
@@ -15,25 +15,29 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         
         if (pollutant != "nitrate" && pollutant != "sulfate") {
                 print("Need to pick 'nitrate' or 'sulfate' for 'pollutant'")
-                return
+                ## return()
+                ## stop()
+        }else{
+        
+                x <- data.frame(date = character(0), sulfate = numeric(0), nitrate = numeric(0), ID = integer(0))
                 
-        }
-        
-        x <- data.frame(date = character(0), sulfate = numeric(0), nitrate = numeric(0), ID = integer(0))
-        
-        for (i in id) {
-                if (i < 10) {
-                        fileN <- paste("00",i,sep="")
-                } else if (i < 99){
-                        fileN <- paste("0",i,sep="")
-                } else {
-                        fileN <- paste("",i,sep="")
+                for (i in id) {
+                        if (i < 10) {
+                                fileN <- paste("00",i,sep="")
+                        } else if (i < 99){
+                                fileN <- paste("0",i,sep="")
+                        } else {
+                                fileN <- paste("",i,sep="")
+                        }
+                        
+                        path <- paste("~/R_code/",directory,"/",fileN,".csv",sep="")
+                        x <- rbind(x,read.csv(path))
+                        ## View(x)
+                        
                 }
-                
-                path <- paste("~/R_code/",directory,"/",fileN,".csv",sep="")
-                x <- rbind(x,read.csv(path))
-                View(x)
-                
+                y <- x[,pollutant]
+                takeout <- is.na(y)
+                ## View(y[!takeout])
+                return (format(mean(y[!takeout]),digits=4))
         }
-        
 }
